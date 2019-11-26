fd0042c83ff5663f0be9483c5a26d4629369b602
Rhian <hellorhian23@gmail.com>
2019-11-26 19:25:35 +0900

    order model (date)

---
+++ api/models/orders.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const productSchema = mongoose.Schema({
+    product : {
+        type : mongoose.Schema.Types.ObjectId,
+        ref : "product",
+        required : true
+    },
+    qty : {
+        type : Number,
+        default : 1
+    },
+    date : {
+        type : Date,
+        default : Date.now
+    },
+
+});
+
+module.exports = mongoose.model("order", orderSchema);
