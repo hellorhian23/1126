6689b439328a3ace7cb2dc07997864b8f93cd7be
Rhian <hellorhian23@gmail.com>
2019-11-26 16:05:58 +0900

    req successful

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const express = require("express");
 
 const app = express();
 
+app.use((req, res) => {
+        res.json({
+            msg: "successful"
+        });
+});
 const port = 4000;
 
 app.listen(port, console.log("server started"));
