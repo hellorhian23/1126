e0ac2ab6ac40944a0c957514bb34a7027885d226
Rhian <hellorhian23@gmail.com>
2019-11-26 17:16:41 +0900

    add mongoose

--- server.js
+++ server.js
@@ -2,11 +2,25 @@ const express = require("express");
 
 const app = express();
 
+const mongoose = require("mongoose");
+
+//라우터루트
+
 const productsRouter = require("./api/routes/products");
 
 const orderRouter = require("./api/routes/orders")
 
 const userRouter = require("./api/routes/users");
+
+//DB 연결
+
+const db_url = "mongodb+srv://Rhian:1234@cluster0-h6nuf.mongodb.net/test?retryWrites=true&w=majority"
+
+
+mongoose.connect(db_url, {useUnifiedTopology: true, useNewUrlParser: true})
+        .then(() => console.log("db connected ... "))
+        .catch(err => console.log(err.message));
+
 
 app.use("/orders", orderRouter);
 
