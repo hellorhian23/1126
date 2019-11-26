465e8882397646b809b1dc82a1178b41fa98eb30
Rhian <hellorhian23@gmail.com>
2019-11-26 16:28:26 +0900

    router

--- server.js
+++ server.js
@@ -2,11 +2,20 @@ const express = require("express");
 
 const app = express();
 
-app.use((req, res) => {
-        res.json({
-            msg: "successful"
-        });
-});
+const productsRouter = require("./api/routes/products");
+
+const orderRouder = require("./api/routes/orders")
+
+app.use("/orders, orderRouter");
+
+app.use("/products", productsRouter);
+
+// app.use((req, res) => {
+//         res.json({
+//             msg: "successful"
+//         });
+// });
 const port = 4000;
 
 app.listen(port, console.log("server started"));
+
